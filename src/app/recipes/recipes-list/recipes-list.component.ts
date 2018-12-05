30d6408d6ab9c0b3291e12ab96ca57414e58f33c
Frank712 <franksuriel_7@hotmail.com>
2018-12-04 22:06:04 -0600

    Views for components created and bootstrap version changed

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -8,7 +8,8 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipesListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('A recipe test', 'This is a description for testing', 'https://www.google.com.mx/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwimtYzDzIffAhWIna0KHdxuAa0QjRx6BAgBEAU&url=https%3A%2F%2Fwww.hellofresh.ca%2Frecipes%2F&psig=AOvVaw3EnpPGR9lXejRk_kAxTRLx&ust=1544061770320465')
+    new Recipe('A recipe test', 'This is a description for testing', `assets/img/food_1.jpg`),
+    new Recipe('An other recipe test', 'This is a description for other testing', `assets/img/food_2.jpg`)
   ];
   constructor() { }
 
